7beb1c22085690c74b13cf0c52956859ac717fba
Jonathan Tremesaygues <jonathan.temesaygues@menta.fr>
2014-07-01 09:27:20 +0200

    possibilité de lire sur l'entrée standard

--- jsonbeautifier.py
+++ jsonbeautifier.py
@@ -1,12 +1,13 @@
 import json
 import pprint
+import sys
 
 import clize
 
 
 @clize.clize
-def main(input_file, output_file=None):
-    with open(input_file, 'r') as input_handler:
+def main(input_file=None):
+    with open(input_file, 'r') if input_file is not None else sys.stdin as input_handler:
         pprint.pprint(json.load(input_handler))
 
 if __name__ == '__main__':
